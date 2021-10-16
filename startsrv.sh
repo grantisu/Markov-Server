7e1be114598fd96092af019c47b616c8f98e1e87
Grant Mathews <grant.m.mathews@gmail.com>
2021-10-15 23:30:02 -0700

    Add systemd unit file

--- startsrv.sh
+++ startsrv.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# DEPRECATED: use Markov-Server.service as a unit file instead
+
 #source ~/perl5/perlbrew/etc/bashrc
 plackup -o localhost -p 5000 -E deployment -s Starman --access-log access.log --workers=1 --max-requests=50000
 
