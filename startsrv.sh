49a5c8985877e8135f54c4dbe213be715247dba1
Grant Mathews <grant.m.mathews@gmail.com>
2021-10-15 19:41:57 -0700

    Copy over startsrv.sh from VPS

--- startsrv.sh
+++ startsrv.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-source ~/perl5/perlbrew/etc/bashrc
-plackup -o localhost -p 5000 -E deployment -s Starman
+#source ~/perl5/perlbrew/etc/bashrc
+plackup -o localhost -p 5000 -E deployment -s Starman --access-log access.log --workers=1 --max-requests=50000
 
